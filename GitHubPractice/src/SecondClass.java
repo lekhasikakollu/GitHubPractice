a5dfb9169b10a7174d0ee9179f547cecd331a907
lekhasikakollu <lekha.sikakollu@gmail.com>
2017-08-25 12:09:15 -0700

    Second Sysout added

--- GitHubPractice/src/SecondClass.java
+++ GitHubPractice/src/SecondClass.java
@@ -4,6 +4,7 @@ public class SecondClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println(" klkefrjjjjjggfj");
+	System.out.println("next sysout");
 	}
 
 }
