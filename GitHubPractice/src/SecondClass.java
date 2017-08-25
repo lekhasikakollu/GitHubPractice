c27b06e1c67fac909a590108863a0b4a187127b1
lekhasikakollu <lekha.sikakollu@gmail.com>
2017-08-25 16:58:34 -0700

    from master with branch 1 and branch2

--- GitHubPractice/src/SecondClass.java
+++ GitHubPractice/src/SecondClass.java
@@ -5,6 +5,7 @@ public class SecondClass {
 		// TODO Auto-generated method stub
 System.out.println(" klkefrjjjjjggfj");
 	System.out.println("next sysout");
+	System.out.println("Branch 1 Sysout");
 	}
 
 }
