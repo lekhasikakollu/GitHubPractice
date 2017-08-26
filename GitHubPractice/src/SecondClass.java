5dded095a8125375b8365462d8ef5f8e068548d8
lekhasikakollu <lekha.sikakollu@gmail.com>
2017-08-26 15:06:29 -0700

    second class modified

--- GitHubPractice/src/SecondClass.java
+++ GitHubPractice/src/SecondClass.java
@@ -5,12 +5,12 @@ public class SecondClass {
 		// TODO Auto-generated method stub
 System.out.println(" klkefrjjjjjggfj");
 	System.out.println("next sysout");
-<<<<<<< HEAD
+
 	System.out.println("Branch 1 Sysout");
-=======
+
 	System.out.println("Branch 2 Second class");
 	
->>>>>>> d09271dbd48916cd78eef10031ef0b6ad3d0828d
+
 	}
 
 }
