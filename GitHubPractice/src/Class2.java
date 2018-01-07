b2be31e8716f0e0dd9e12036d6029eabe709b139
lekhasikakollu <lekha.sikakollu@gmail.com>
2018-01-07 11:41:39 -0800

    Adding Class2.java

---
+++ GitHubPractice/src/Class2.java
@@ -0,0 +1,12 @@
+
+public class Class2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println(" From Class 2");
+	System.out.println("next sysout");
+	System.out.println("Branch 1 Class2");
+	
+	}
+
+}
